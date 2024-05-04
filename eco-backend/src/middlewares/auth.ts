341263fe9c8f49edbf7e6074f3701feadd395d35
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2024-05-04 12:28:02 +0530

    add auth middleware

---
+++ eco-backend/src/middlewares/auth.ts
@@ -0,0 +1,23 @@
+import { User } from "../models/User.Model.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "./error.js";
+
+
+// middleware to make sure only admin is allowed
+export const adminOnly = () => TryCatch(async(req, res, next) => {
+
+    const {id} = req.query;
+    if(!id){
+        return next(new ErrorHandler("Login required", 401))
+
+    }
+    const user = await User.findById(id);
+    if(!user){
+        return next(new ErrorHandler("User not found", 401))
+    }
+    if(user.role !== 'admin'){
+        return next(new ErrorHandler("Request Unautherized", 401))
+    }
+
+    next();
+})
--- eco-backend/src/routes/User.Route.ts
+++ eco-backend/src/routes/User.Route.ts
@@ -1,16 +1,16 @@
 import express from 'express'
 import { deleteUser, getAllUsers, getUser, newUser } from '../controllers/User.controller.js';
+import { adminOnly } from '../middlewares/auth.js';
 
 const UserRoute = express.Router()
 
-// get routes
-UserRoute.get('/all', getAllUsers);
 
 // post routes
 UserRoute.post('/new', newUser)
 
-//delete routes
-UserRoute.delete('/:id', deleteUser)
+// protected routes
+UserRoute.get('/all',  adminOnly, getAllUsers)   //get
+UserRoute.delete('/:id',adminOnly, deleteUser)  //delete
 
 // dynamic routes
 UserRoute.get('/:id', getUser);
